56872def8a0a7db04cab7bd5f58ed49b4d0b1d87
liuguangqi <liuguangqi@bytedance.com>
2020-09-20 17:13:32 +0800

    version update

--- src/components/FAvatar/index.tsx
+++ src/components/FAvatar/index.tsx
@@ -27,7 +27,7 @@ const FAvatar: React.FC<Props> = ({ src, ...restProps }) => {
   return url ? (
     <Avatar {...restProps} src={getFavicon(url)} />
   ) : (
-    <SmileOutlined />
+    <SmileOutlined style={{ width: 32 }} {...restProps} />
   );
 };
 
