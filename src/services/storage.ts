8be6d953a68fcbd2549b807d96c62e8c9735c5a0
liuguangqi <liuguangqi@bytedance.com>
2020-09-20 14:32:52 +0800

    version update

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,7 +1,14 @@
 import { promisify } from "../utils";
 
 class Storage {
-  static set = promisify(chrome.storage.local.set);
+  static set = (str: string | string[]) => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(str, (items) => {
+        console.log("===", items);
+        resolve(items);
+      });
+    });
+  };
   static get = promisify(chrome.storage.local.get);
 }
 
