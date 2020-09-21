2a262a29b5031508b466e029e871595d8556fad9
liuguangqi <liuguangqi@bytedance.com>
2020-09-21 18:14:36 +0800

    version update

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useCallback, useEffect, useState } from "react";
 
-import { Layout, Menu } from "antd";
+import { Layout, Menu, message } from "antd";
 
 import Bookmark from "../../services/bookmark";
 import appInfo from "../../../package.json";
@@ -61,6 +61,23 @@ const Home: React.FC = () => {
   chrome.bookmarks.onRemoved.addListener(getTreeBookmarks);
   chrome.bookmarks.onCreated.addListener(getTreeBookmarks);
 
+  const onDrop = async (ev: any, side) => {
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move";
+    const movedBookmark = JSON.parse(ev.dataTransfer.getData("movedBookmark"));
+    try {
+      await Bookmark.move(movedBookmark.id, {
+        parentId: side.id,
+      });
+    } catch (e) {
+      message.error(e.message);
+    }
+  };
+
+  const onDragOver = (ev: any) => {
+    ev.preventDefault();
+  };
+
   const renderMenu = (sideItems): ReactNode => {
     return sideItems.map((side) => {
       if (side.children && side.children.length > 0) {
@@ -73,6 +90,8 @@ const Home: React.FC = () => {
         return (
           <Menu.Item
             key={side.id}
+            onDrop={(e) => onDrop(e, side)}
+            onDragOver={onDragOver}
             onClick={() => scrollIntoView(`${side.title}_${side.id}`)}
           >
             {side.title}
