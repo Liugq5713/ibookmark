feefade66c1ce3232b0b00d0e155cafd1d319bb7
liuguangqi <liuguangqi@bytedance.com>
2020-09-28 17:53:33 +0800

    version update

--- src/views/Bookmarks/BookmarkNode/index.tsx
+++ src/views/Bookmarks/BookmarkNode/index.tsx
@@ -147,7 +147,7 @@ const Text = styled.a`
 `;
 
 const OptWrapper = styled.div.attrs({
-  class: "left-0 top-0 pt-2 pr-2 absolute",
+  className: "left-0 top-0 pt-2 pr-2 absolute",
 })`
   display: none;
   ${Content}:hover & {
