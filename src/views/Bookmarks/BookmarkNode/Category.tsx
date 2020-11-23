a08f01d8d161127474d023e4a7db42531479eba8
liuguangqi <liuguangqi@bytedance.com>
2020-11-23 14:55:23 +0800

    version update

--- src/views/Bookmarks/BookmarkNode/Category.tsx
+++ src/views/Bookmarks/BookmarkNode/Category.tsx
@@ -68,7 +68,7 @@ const TitleLink = styled.div`
     content: " ";
     display: block;
     position: absolute;
-    top: 28px;
+    top: 44px;
     left: 0;
     border-right: 10px solid #193d61;
     border-bottom: 10px dashed transparent;
